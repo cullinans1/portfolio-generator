033ea5df7a429764262f9e9b60f8521be4338b6e
Sammy <sammy.cullinan@gmail.com>
2020-07-23 10:19:06 -0700

    inquirer set up

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-const [name, github] = profileDataArgs;
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
+// const profileDataArgs = process.argv.slice(2, process.argv.length);
+// const [name, github] = profileDataArgs;
 
-fs.writeFile('./index.html', generatePage(name, github), err => {
-    if (err) throw err;
-    console.log("Portfolio Complete! Checkout index.html to see the output.")
-});
+// fs.writeFile('./index.html', generatePage(name, github), err => {
+//     if (err) throw err;
+//     console.log("Portfolio Complete! Checkout index.html to see the output.")
+// });
 
+const inquirer = require('inquirer');
+inquirer
+    .prompt ([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+    .then (answers => console.log(answers));
